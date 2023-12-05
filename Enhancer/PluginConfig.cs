6952c799e73e9929a69c9c4657a2df8b3a5035da
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2023-12-05 05:46:28 +0000

    rename 'TimeScale' -> 'TimeSpeed', set default to 1.0f, variable acts as a multiplier.
    Closes #3

--- Enhancer/PluginConfig.cs
+++ Enhancer/PluginConfig.cs
@@ -14,7 +14,7 @@ public class PluginConfig
     public readonly bool UseRandomPrices;
     public readonly bool SuitUnlocksEnabled;
 
-    public readonly float TimeScale;
+    public readonly float TimeSpeed;
     public readonly float MinimumBuyRate;
     public readonly float DoorTimer;
 
@@ -38,7 +38,7 @@ public class PluginConfig
         UseRandomPrices = BindingPlugin.Config.Bind(PluginInfo.PLUGIN_GUID, "bUseRandomPrices", false, "Enables the random prices setting. Great if you're using longer quota deadlines.\nThis uses a variety of things to randomize prices such as the company mood, time passed in the quota, etc.\nRespects the minimum sale value, too.\nHost Required: Yes").Value;
         SuitUnlocksEnabled = BindingPlugin.Config.Bind(PluginInfo.PLUGIN_GUID, "bUnlockSuits", false, "Unlocks a few of the cheaper suits from the start so your crew has options.\nHost Required: Yes").Value;
 
-        TimeScale = BindingPlugin.Config.Bind(PluginInfo.PLUGIN_GUID, "fTimeScale", 1.5f, "How fast time passes on moons. Lower values mean time passes more slowly.\nRecommended value for single play: 1.15\nHost Required: Yes").Value;
+        TimeSpeed = BindingPlugin.Config.Bind(PluginInfo.PLUGIN_GUID, "fTimeScale", 1.0f, "How fast time passes on moons. Lower values mean time passes more slowly.\nRecommended value for single play: 0.75\nHost Required: Yes").Value;
         MinimumBuyRate = BindingPlugin.Config.Bind(PluginInfo.PLUGIN_GUID, "fMinCompanyBuyPCT", 0.0f, "The default formula for selling items to the company doesn't allow days remaining above 3.\nAlways keep this set to at least 0.0 but you probably want something higher if you have more days set for the quota.\nRecommended values for games above 3 days: 0.3 - 0.5\nHost Required: Yes").Value;
         DoorTimer = BindingPlugin.Config.Bind(PluginInfo.PLUGIN_GUID, "fDoorTimer", 30.0f, "How long the hangar door can be kept shut at a time (in seconds)\nRecommended values: 60.0 - 180.0\nHost Required: All players should use the same setting here").Value;
 
