ed5ba2e9447f63ae1214b539c460ab6529bab7f1
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2023-12-05 02:59:06 +0000

    implement fluent builder pattern for `PatchInfo`; block all patches if llama.enhancer is preseent

--- Enhancer/Plugin.cs
+++ Enhancer/Plugin.cs
@@ -4,53 +4,79 @@
     Authors:
         Mama Llama
         Flowerful
+        Lordfirespeed
 
-    See Docs/License for information about copying
+    See LICENSE.md for information about copying
     distributing this project
 
-    See Docs/Installing for information on how to
-    use this mod in your game
+    See Docs/Installation.md for information on 
+    how to use this mod in your game
 ***********************************************************/
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using BepInEx;
+using BepInEx.Bootstrap;
 using BepInEx.Logging;
 using Enhancer.Patches;
 using HarmonyLib;
+using UnityEngine.UIElements.Collections;
 
 namespace Enhancer;
 
-[BepInPlugin("com.github.lordfirespeed.augmented_enhancer", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+[BepInDependency("mom.llama.enhancer", BepInDependency.DependencyFlags.SoftDependency)]
 public class Plugin : BaseUnityPlugin
 {
-    public static ManualLogSource Log;
-    public static PluginConfig BoundConfig;
+    public static ManualLogSource Log { get; private set; }
+    public static PluginConfig BoundConfig { get; private set; }
+    
+    private static PatchInfo[] _patches = new[]
+    {
+        new PatchInfo.Builder()
+            .SetName("Configured values")
+            .SetPatchType(typeof(ConfiguredValues))
+            .AddModGuidToDelegateTo("mom.llama.enhancer")
+            .Build(),
+        new PatchInfo.Builder()
+            .SetName("Improved scan command")
+            .SetPatchType(typeof(ImprovedScanCommand))
+            .AddModGuidToDelegateTo("mom.llama.enhancer")
+            .Build(),
+        new PatchInfo.Builder()
+            .SetName("Item protection")
+            .SetPatchType(typeof(ItemProtection))
+            .AddModGuidToDelegateTo("mom.llama.enhancer")
+            .Build(),
+        new PatchInfo.Builder()
+            .SetName("Price randomizer")
+            .SetPatchType(typeof(PriceRandomizer))
+            .AddModGuidToDelegateTo("mom.llama.enhancer")
+            .Build(),
+        new PatchInfo.Builder()
+            .SetName("Suit unlocks")
+            .SetPatchType(typeof(SuitUnlockables))
+            .SetLoadCondition(() => BoundConfig.SuitUnlocksEnabled)
+            .AddModGuidToDelegateTo("mom.llama.enhancer")
+            .Build(),
+    };
+    
     private void Awake()
     {
-        // Plugin startup logic
         Log = Logger;
-
         BoundConfig = new(this);
 
         if (!BoundConfig.Enabled)
         {
-            Logger.LogInfo("Globally disabled, exit");
+            Logger.LogInfo("Globally disabled, exiting. Goodbye!");
             return;
         }
 
         Harmony patcher = new(PluginInfo.PLUGIN_GUID);
-
-        var patches = new PatchInfo[]
-        {
-            new("Configured values", typeof(ConfiguredValues)),
-            new("Improved scan command", typeof(ImprovedScanCommand)),
-            new("Item protection", typeof(ItemProtection)),
-            new("Price randomizer", typeof(PriceRandomizer)),
-            new("Suit unlockables", typeof(SuitUnlockables), loadCondition: () => BoundConfig.SuitUnlocksEnabled),
-        };
         
-        Logger.LogInfo("Enabled, applying all patches");
-        foreach (var patch in patches)
+        Logger.LogInfo("Enabled, applying patches");
+        foreach (var patch in _patches)
         {
             if (!patch.ShouldLoad()) continue;
             Logger.LogInfo($"Applying {patch.Name} patches...");
@@ -58,13 +84,62 @@ public class Plugin : BaseUnityPlugin
         }
     }
 
-    private class PatchInfo {
-        public PatchInfo(string name, Type patchType, Func<bool>? loadCondition = null) => 
-            (Name, PatchType, LoadCondition) = (name, patchType, loadCondition);
-        
-        public string Name { get; }
-        public Type PatchType { get; }
-        private Func<bool> LoadCondition;
-        public bool ShouldLoad() => LoadCondition == null || LoadCondition();
+    private class PatchInfo
+    {
+        public string Name { get; private set; }
+        public Type PatchType { get; private set; }
+        private Func<bool> _loadCondition;
+        private string[] _delegateToModGuids;
+
+        public bool ShouldLoad() => (_loadCondition == null || _loadCondition()) && !HasLoadedDelegate();
+        public bool HasLoadedDelegate()
+        {
+            var delegateToPluginInfosEnumerable = from delegateToModGuid in _delegateToModGuids 
+                select Chainloader.PluginInfos.Get(delegateToModGuid);
+            var delegateToPluginInfos = delegateToPluginInfosEnumerable as BepInEx.PluginInfo[] ?? delegateToPluginInfosEnumerable.ToArray();
+            if (!delegateToPluginInfos.Any()) return false;
+            Log.LogWarning($"{Name} feature is disabled due to the presence of '{String.Join(", ", delegateToPluginInfos.Select(info => info.Metadata.Name))}'");
+            return true;
+        }
+
+        public class Builder
+        {
+            private string _thisName;
+            private Type _thisPatchType;
+            private Func<bool> _thisLoadCondition;
+            private List<string> _thisDelegateToModGuids = new();
+            
+            public Builder SetName(string newName)
+            {
+                 _thisName = newName;
+                 return this;
+            }
+
+            public Builder SetPatchType(Type newPatchType)
+            {
+                _thisPatchType = newPatchType;
+                return this;
+            }
+
+            public Builder SetLoadCondition(Func<bool> loadCondition)
+            {
+                _thisLoadCondition = loadCondition;
+                return this;
+            }
+
+            public Builder AddModGuidToDelegateTo(string delegateToModGuid)
+            {
+                _thisDelegateToModGuids.Add(delegateToModGuid);
+                return this;
+            }
+
+            public PatchInfo Build() => new()
+            {
+                Name = _thisName,
+                PatchType = _thisPatchType,
+                _loadCondition = _thisLoadCondition,
+                _delegateToModGuids = _thisDelegateToModGuids.ToArray(),
+            };
+        }
     }
 }
