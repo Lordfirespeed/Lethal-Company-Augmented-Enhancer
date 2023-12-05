1a3cd5c7a58e460665f2be260e2bb7baf4490f1b
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2023-12-05 05:46:46 +0000

    rename DoorTimer -> DoorPowerDuration

--- Enhancer/PluginConfig.cs
+++ Enhancer/PluginConfig.cs
@@ -16,7 +16,7 @@ public class PluginConfig
 
     public readonly float TimeSpeed;
     public readonly float MinimumBuyRate;
-    public readonly float DoorTimer;
+    public readonly float DoorPowerDuration;
 
     public readonly bool StartingCreditsEnabled;
     public readonly int StartingCredits;
@@ -40,7 +40,7 @@ public class PluginConfig
 
         TimeSpeed = BindingPlugin.Config.Bind(PluginInfo.PLUGIN_GUID, "fTimeScale", 1.0f, "How fast time passes on moons. Lower values mean time passes more slowly.\nRecommended value for single play: 0.75\nHost Required: Yes").Value;
         MinimumBuyRate = BindingPlugin.Config.Bind(PluginInfo.PLUGIN_GUID, "fMinCompanyBuyPCT", 0.0f, "The default formula for selling items to the company doesn't allow days remaining above 3.\nAlways keep this set to at least 0.0 but you probably want something higher if you have more days set for the quota.\nRecommended values for games above 3 days: 0.3 - 0.5\nHost Required: Yes").Value;
-        DoorTimer = BindingPlugin.Config.Bind(PluginInfo.PLUGIN_GUID, "fDoorTimer", 30.0f, "How long the hangar door can be kept shut at a time (in seconds)\nRecommended values: 60.0 - 180.0\nHost Required: All players should use the same setting here").Value;
+        DoorPowerDuration = BindingPlugin.Config.Bind(PluginInfo.PLUGIN_GUID, "fDoorTimer", 30.0f, "How long the hangar door can be kept shut at a time (in seconds)\nRecommended values: 60.0 - 180.0\nHost Required: All players should use the same setting here").Value;
 
         StartingCreditsEnabled = BindingPlugin.Config.Bind(
             PluginInfo.PLUGIN_GUID,
