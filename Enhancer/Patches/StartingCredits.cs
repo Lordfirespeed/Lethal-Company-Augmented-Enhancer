8f4bf3fe3592253a8ce9ac0b1b2b199052009e49
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-12-21 04:14:02 +0000

    fix starting credits patch

--- Enhancer/Patches/StartingCredits.cs
+++ Enhancer/Patches/StartingCredits.cs
@@ -4,9 +4,9 @@ namespace Enhancer.Patches;
 
 public class StartingCredits
 {
-    [HarmonyPatch(typeof(StartOfRound), "Start")]
+    [HarmonyPatch(typeof(Terminal), "Start")]
     [HarmonyPrefix]
-    public static void StartOfRoundShipStartPre()
+    public static void TerminalStartPre()
     {
         Plugin.Log.LogInfo($"Setting starting credits to {Plugin.BoundConfig.StartingCredits.Value}...");
         var quotaSettings = TimeOfDay.Instance.quotaVariables;
