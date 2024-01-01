1a3d49478a641f18b7693faa83d806582463ee60
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2024-01-01 20:15:58 +0000

    run formatter, add missing log-lines, switch `Plugin.Logger` refs to `Logger`

--- Enhancer/Patches/ScrapTweaks.cs
+++ Enhancer/Patches/ScrapTweaks.cs
@@ -13,7 +13,7 @@ public class ScrapTweaks : IPatch
         logger.LogDebug("Logger assigned.");
         Logger = logger;
     }
-    
+
     private static float _originalScrapValueMultiplier = 1f;
     private static float _originalScrapAmountMultiplier = 1f;
 
@@ -27,7 +27,7 @@ public class ScrapTweaks : IPatch
     public void OnPatch()
     {
         if (!RoundManagerExistsAndIsServer()) return;
-        
+
         SubscribeToEvents();
         CacheMultipliers(RoundManager.Instance);
         ApplyMultipliers(RoundManager.Instance.playersManager.connectedPlayersAmount);
@@ -36,14 +36,14 @@ public class ScrapTweaks : IPatch
     public void OnConfigChange()
     {
         if (!RoundManagerExistsAndIsServer()) return;
-        
+
         ApplyMultipliers(RoundManager.Instance.playersManager.connectedPlayersAmount);
     }
 
     public void OnUnpatch()
     {
         if (!RoundManagerExistsAndIsServer()) return;
-        
+
         UnsubscribeFromEvents();
         RestoreMultipliers(RoundManager.Instance);
     }
@@ -54,54 +54,73 @@ public class ScrapTweaks : IPatch
     {
         Logger.LogDebug("Round manager started!");
         if (__instance is { IsHost: false, IsServer: false }) return;
-        Logger.LogDebug("I am the server!");
-        
+
         SubscribeToEvents();
         CacheMultipliers(__instance);
         ApplyMultipliers(__instance.playersManager.connectedPlayersAmount);
     }
-    
+
     [HarmonyPatch(typeof(RoundManager), nameof(RoundManager.OnDestroy))]
     [HarmonyPrefix]
     public static void RoundManagerDestruction(RoundManager __instance)
     {
-        Plugin.Logger.LogDebug("Round manager destroying!");
+        Logger.LogDebug("Round manager destroying!");
         if (__instance is { IsHost: false, IsServer: false }) return;
-        Plugin.Logger.LogDebug("I am the server!");
-        
+
         UnsubscribeFromEvents();
     }
 
     private static void SubscribeToEvents()
     {
-        Logger.LogDebug("Subscribed to join/leave!");
         LC_API.GameInterfaceAPI.Events.Handlers.Player.Joined += OnPlayerJoin;
         LC_API.GameInterfaceAPI.Events.Handlers.Player.Left += OnPlayerLeave;
+        Logger.LogDebug("Subscribed to join/leave!");
     }
 
     private static void UnsubscribeFromEvents()
     {
         LC_API.GameInterfaceAPI.Events.Handlers.Player.Joined -= OnPlayerJoin;
         LC_API.GameInterfaceAPI.Events.Handlers.Player.Left -= OnPlayerLeave;
+        Logger.LogDebug("Unsubscribed from join/leave!");
     }
 
     private static void CacheMultipliers(RoundManager manager)
     {
-        _originalScrapValueMultiplier = manager.scrapValueMultiplier;
-        _originalScrapAmountMultiplier = manager.scrapAmountMultiplier;
+        try
+        {
+            _originalScrapValueMultiplier = manager.scrapValueMultiplier;
+            _originalScrapAmountMultiplier = manager.scrapAmountMultiplier;
+        }
+        finally
+        {
+            Logger.LogDebug(
+                $"Attempted to cache scrap value multiplier. " + 
+                $"Value is currently {RoundManager.Instance.scrapValueMultiplier}"
+            );
+            Logger.LogDebug(
+                $"Attempted to cache scrap quantity multiplier. " +
+                $"Value is currently {RoundManager.Instance.scrapAmountMultiplier}"
+            );
+        }
     }
 
     private static void RestoreMultipliers(RoundManager manager)
     {
         try
         {
-            manager.scrapValueMultiplier = _originalScrapValueMultiplier; 
+            manager.scrapValueMultiplier = _originalScrapValueMultiplier;
             manager.scrapAmountMultiplier = _originalScrapAmountMultiplier;
         }
         finally
         {
-            Logger.LogDebug($"Attempted to restore scrap value multiplier. Value is now {RoundManager.Instance.scrapValueMultiplier}");
-            Logger.LogDebug($"Attempted to restore scrap quantity multiplier. Value is now {RoundManager.Instance.scrapAmountMultiplier}");
+            Logger.LogDebug(
+                $"Attempted to restore scrap value multiplier. " + 
+                $"Value is now {RoundManager.Instance.scrapValueMultiplier}"
+            );
+            Logger.LogDebug(
+                $"Attempted to restore scrap quantity multiplier. " +
+                $"Value is now {RoundManager.Instance.scrapAmountMultiplier}"
+            );
         }
     }
 
@@ -127,22 +146,32 @@ public class ScrapTweaks : IPatch
         if (!RoundManager.Instance) return;
         try
         {
-            RoundManager.Instance.scrapValueMultiplier = _originalScrapValueMultiplier * Plugin.BoundConfig.ScrapValueScalar.Value;
-            RoundManager.Instance.scrapAmountMultiplier = _originalScrapAmountMultiplier * Plugin.BoundConfig.ScrapQuantityScalar.Value;
+            RoundManager.Instance.scrapValueMultiplier =
+                _originalScrapValueMultiplier * Plugin.BoundConfig.ScrapValueScalar.Value;
+            RoundManager.Instance.scrapAmountMultiplier =
+                _originalScrapAmountMultiplier * Plugin.BoundConfig.ScrapQuantityScalar.Value;
 
             if (playerCount <= 4 || Plugin.BoundConfig.ScrapPlayercountScaling.Value == 0f) return;
 
             var extraPlayers = playerCount - 4;
 
             RoundManager.Instance.scrapValueMultiplier +=
-                RoundManager.Instance.scrapValueMultiplier * Plugin.BoundConfig.ScrapPlayercountScaling.Value * extraPlayers / 4;
+                RoundManager.Instance.scrapValueMultiplier * Plugin.BoundConfig.ScrapPlayercountScaling.Value *
+                extraPlayers / 4;
             RoundManager.Instance.scrapAmountMultiplier -=
-                RoundManager.Instance.scrapValueMultiplier * Plugin.BoundConfig.ScrapPlayercountScaling.Value * extraPlayers / playerCount;
+                RoundManager.Instance.scrapValueMultiplier * Plugin.BoundConfig.ScrapPlayercountScaling.Value *
+                extraPlayers / playerCount;
         }
         finally
         {
-            Plugin.Logger.LogDebug($"Attempted to update scrap value multiplier. Value is now {RoundManager.Instance.scrapValueMultiplier}");
-            Plugin.Logger.LogDebug($"Attempted to update scrap quantity multiplier. Value is now {RoundManager.Instance.scrapAmountMultiplier}");
+            Logger.LogDebug(
+                $"Attempted to update scrap value multiplier. " + 
+                $"Value is now {RoundManager.Instance.scrapValueMultiplier}"
+            );
+            Logger.LogDebug(
+                $"Attempted to update scrap quantity multiplier. " +
+                    $"Value is now {RoundManager.Instance.scrapAmountMultiplier}"
+            );
         }
     }
 }
