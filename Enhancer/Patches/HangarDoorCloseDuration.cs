4d876d04845489e2655cf3edcc338133f727eec0
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2024-01-01 11:42:02 +0000

    HangarDoorCloseDuration has its own logger

--- Enhancer/Patches/HangarDoorCloseDuration.cs
+++ Enhancer/Patches/HangarDoorCloseDuration.cs
@@ -1,14 +1,23 @@
+using BepInEx.Logging;
 using HarmonyLib;
 
 namespace Enhancer.Patches;
 
 public class HangarDoorCloseDuration : IPatch
 {
+    protected static ManualLogSource Logger { get; set; } = null!;
+
+    public void SetLogger(ManualLogSource logger)
+    {
+        logger.LogDebug("Logger assigned.");
+        Logger = logger;
+    }
+    
     [HarmonyPatch(typeof(HangarShipDoor), "Start")]
     [HarmonyPostfix]
     public static void HangarShipDoorPost(HangarShipDoor __instance)
     {
-        Plugin.Logger.LogInfo($"Setting Hangar door power to last for {Plugin.BoundConfig.DoorPowerDuration.Value} seconds...");
+        Logger.LogInfo($"Setting Hangar door power to last for {Plugin.BoundConfig.DoorPowerDuration.Value} seconds...");
         __instance.doorPowerDuration = Plugin.BoundConfig.DoorPowerDuration.Value;
     }
 }
