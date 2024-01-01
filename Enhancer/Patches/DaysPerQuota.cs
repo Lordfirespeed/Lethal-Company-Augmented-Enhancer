fb9f9f6e585dceffd3c84941bec9fd7102f1f123
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2024-01-01 11:38:00 +0000

    days per quota has its own logger

--- Enhancer/Patches/DaysPerQuota.cs
+++ Enhancer/Patches/DaysPerQuota.cs
@@ -1,14 +1,23 @@
+using BepInEx.Logging;
 using HarmonyLib;
 
 namespace Enhancer.Patches;
 
 public class DaysPerQuota : IPatch
 {
+    protected static ManualLogSource Logger { get; set; } = null!;
+
+    public void SetLogger(ManualLogSource logger)
+    {
+        logger.LogDebug("Logger assigned.");
+        Logger = logger;
+    }
+    
     [HarmonyPatch(typeof(StartOfRound), "Start")]
     [HarmonyPrefix]
     public static void StartOfRoundShipStartPre()
     {
-        Plugin.Logger.LogInfo($"Setting days per quota to {Plugin.BoundConfig.DaysPerQuotaAssignment.Value}...");
+        Logger.LogInfo($"Setting days per quota to {Plugin.BoundConfig.DaysPerQuotaAssignment.Value}...");
         var quotaSettings = TimeOfDay.Instance.quotaVariables;
         quotaSettings.deadlineDaysAmount = Plugin.BoundConfig.DaysPerQuotaAssignment.Value;
     }
