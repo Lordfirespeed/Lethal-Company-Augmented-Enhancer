410fcf5d4f0f7eac84d3a26de3054ded038eae24
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2024-01-01 11:43:24 +0000

    passive income has its own logger

--- Enhancer/Patches/PassiveIncome.cs
+++ Enhancer/Patches/PassiveIncome.cs
@@ -1,16 +1,25 @@
+using BepInEx.Logging;
 using HarmonyLib;
 
 namespace Enhancer.Patches;
 
 public class PassiveIncome : IPatch
 {
+    protected static ManualLogSource Logger { get; set; } = null!;
+
+    public void SetLogger(ManualLogSource logger)
+    {
+        logger.LogDebug("Logger assigned.");
+        Logger = logger;
+    }
+    
     [HarmonyPatch(typeof(TimeOfDay), nameof(TimeOfDay.OnDayChanged))]
     [HarmonyPostfix]
     public static void AddPassiveIncome(TimeOfDay __instance)
     {
         if (!__instance.IsServer) return;
         
-        Plugin.Logger.LogInfo($"Adding {Plugin.BoundConfig.PassiveIncomeQuantity.Value} passive income credits...");
+        Logger.LogInfo($"Adding {Plugin.BoundConfig.PassiveIncomeQuantity.Value} passive income credits...");
         Terminal? objectOfType = UnityEngine.Object.FindObjectOfType<Terminal>();
         objectOfType!.groupCredits += Plugin.BoundConfig.PassiveIncomeQuantity.Value;
         objectOfType.SyncGroupCreditsServerRpc(objectOfType.groupCredits, objectOfType.numberOfItemsInDropship);
