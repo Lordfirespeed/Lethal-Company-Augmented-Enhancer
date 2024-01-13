eaabd7818f1ccef99655b6b40dc1f98eda4f4009
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-01-13 16:53:51 +0000

    write Tap LINQ extension

---
+++ Enhancer/Extensions/CollectionExtensions.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+namespace Enhancer.Extensions;
+
+public static class CollectionExtensions
+{
+    /// <summary>Execute code for every element in a collection and pass-through the original value.</summary>
+    /// <typeparam name="TSource">The inner type of the collection</typeparam>
+    /// <param name="source">The collection</param>
+    /// <param name="action">The action to execute</param>
+    /// <returns>The collection, unchanged</returns>
+    public static IEnumerable<TSource> Tap<TSource>(this IEnumerable<TSource>? source, Action<TSource>? action)
+    {
+        if (source is null) throw new ArgumentNullException(nameof(source));
+        if (action is null) throw new ArgumentNullException(nameof(action));
+        return TapIterator(source, action);
+    }
+
+    static IEnumerable<TSource> TapIterator<TSource>(IEnumerable<TSource> source, Action<TSource> action)
+    {
+        foreach (TSource element in source)
+        {
+            action(element);
+            yield return element;
+        }
+    }
+}
