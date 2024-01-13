7748a9c1d246b775d32ea2a02a94c5e5e22a63e5
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-01-13 16:57:19 +0000

    update log-lines; use `.Tap`; unlock items from configured list

--- Enhancer/Patches/FreeUnlockables.cs
+++ Enhancer/Patches/FreeUnlockables.cs
@@ -1,6 +1,6 @@
-using System.Collections.Generic;
 using System.Linq;
 using BepInEx.Logging;
+using Enhancer.Extensions;
 using HarmonyLib;
 
 namespace Enhancer.Patches;
@@ -15,19 +15,17 @@ public class FreeUnlockables : IPatch
         Logger = logger;
     }
 
-    private static readonly HashSet<string> UnlockableNamesToSpawn = ["Green suit", "Hazard suit"];
-
     [HarmonyPatch(typeof(StartOfRound), nameof(StartOfRound.Start))]
     [HarmonyPostfix]
     public static void StartOfRoundSuitPatch(StartOfRound __instance)
     {
-        Logger.LogInfo("Setting unlocked suits this round");
+        Logger.LogInfo("Unlocking items...");
 
         __instance.unlockablesList.unlockables
             .Select((unlockable, index) => new { unlockable, index })
+            .Tap(item => Logger.LogDebug($"Unlockable item {item.index}: '{item.unlockable.unlockableName}'"))
             .Where(item => {
-                Logger.LogDebug(item.unlockable.unlockableName);
-                return UnlockableNamesToSpawn.Contains(item.unlockable.unlockableName);
+                return Plugin.BoundConfig.FreeUnlockablesList.Value.Contains(item.unlockable.unlockableName);
             })
             .Do(item => __instance.SpawnUnlockable(item.index));
     }
