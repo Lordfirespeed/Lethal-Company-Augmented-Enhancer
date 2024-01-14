2eebbf39e52039474aadd1dde990e1902d65239a
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-01-14 22:53:49 +0000

    add SetHarmony to IFeature

--- Enhancer/Features/IFeature.cs
+++ Enhancer/Features/IFeature.cs
@@ -1,10 +1,12 @@
 using BepInEx.Logging;
+using HarmonyLib;
 
 namespace Enhancer.Features;
 
 public interface IFeature
 {
     public void SetLogger(ManualLogSource logger) { }
+    public void SetHarmony(Harmony harmony) { }
     public void OnEnable() { }
     public void OnDisable() { }
     public void OnConfigChange() { }
